db4b8b25a985819b81ebe9969b2627d8563c294a
RidwanAdebosin <adebosinadewale@gmail.com>
2025-03-22 23:15:26 +0100

    feat: UserDashboard layout implemented

--- src/ui/components/Layout/UserDashboardLayout.jsx
+++ src/ui/components/Layout/UserDashboardLayout.jsx
@@ -5,7 +5,7 @@ import { UserDashboard } from "../../../pages/userDashboard/UserDashboard";
 
 const UserDashboardLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen justify-between">
+    <div className="flex flex-col justify-between">
       <Header />
       <Outlet />
       <CartFooter />
